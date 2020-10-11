ccf08bbeebf5c9078b78cd819adc5453f2045172
anne <anne.marie@strivelabs.io>
2020-10-11 15:29:35 +0200

    frontend: start adding animation and enter listener

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -5,28 +5,42 @@ import { faFire } from "@fortawesome/free-solid-svg-icons";
 import data from "../data/dataSearch.js";
 import Deck from "./Deck";
 import "../styles/Search.css";
+import useKeyPress from "../utils/useKeyPress";
+import Animate from "animate.css-react";
+import "animate.css/animate.css";
 
 const Search = () => {
   const [showDeck, setShowDeck] = useState(false);
+  /*Como le paso esto al componente de input ReactSearchBox? Quiero que al darle a enter haga la busqueda*/
+  const enterPress = useKeyPress("h");
 
   const onClick = () => setShowDeck(true);
 
+  /*No tengo forma de arrancar la animacion, que me falta?*/
   return (
     <div>
-      <div className="Search">
-        <div className="search-wrapper">
-          <button className="button-search" onClick={onClick}>
-            <FontAwesomeIcon icon={faFire} />
-          </button>
-          <div className="search-box">
-            <ReactSearchBox
-              placeholder="Search your movie ♥"
-              data={data}
-              callback={(record) => console.log(record)}
-            />
+      <Animate
+        enter="jackInTheBox"
+        appear="jackInTheBox"
+        durationAppear={2000}
+        durationEnter={2000}
+        component="div"
+      >
+        <div className="Search">
+          <div className="search-wrapper">
+            <button className="button-search" onClick={onClick}>
+              <FontAwesomeIcon icon={faFire} />
+            </button>
+            <div className="search-box">
+              <ReactSearchBox
+                placeholder="Search your movie ♥"
+                data={data}
+                callback={(record) => console.log("record")}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </Animate>
       <div className="Deck-Searched">{showDeck ? <Deck /> : null}</div>
     </div>
   );
