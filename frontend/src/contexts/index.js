c667903f6538c7676a838adc54d818bce067385d
Paolo <str237@gmail.com>
2020-10-22 11:18:56 +0200

    Export provider

--- frontend/src/contexts/index.js
+++ frontend/src/contexts/index.js
@@ -1,3 +1,9 @@
 import React from 'react'
+import { useCoreReducer } from '../reducers'
 
-export const coreContext = React.createContext()
+const CoreContext = React.createContext()
+
+export const CoreContextProvider = ({ children }) => {
+  const stateAndDispatch = useCoreReducer()
+  return <CoreContext.Provider value={stateAndDispatch}>{children}</CoreContext.Provider>
+}
