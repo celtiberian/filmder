91dcc77d01d067d45d6174fd4bfada0d49d0c52a
Paolo <str237@gmail.com>
2020-10-22 11:44:28 +0200

    Fix typo

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,6 +1,6 @@
-import { CORE_ACTIONS } from "./actionNames"
+import { CORE_ACTIONS } from './actionNames'
 
-export const setName = name => ({
+export const setName = (name) => ({
   type: CORE_ACTIONS.SET_USERNAME,
-  payloa: { name },
+  payload: { name },
 })
