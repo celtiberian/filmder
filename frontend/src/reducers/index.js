4c659b2bb54e2da25667b711e37b4f042cb86d0c
Paolo <str237@gmail.com>
2020-10-22 11:06:38 +0200

    Export use reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,3 +1,13 @@
-export const coreReducer = (state, action) => {
-    return state
+import React from 'react'
+
+const coreReducer = (state, action) => {
+  return state
+}
+
+const init = () => {
+
+}
+
+export const useCoreReducer = () => {
+   return React.useReducer(coreReducer, init)
 }
