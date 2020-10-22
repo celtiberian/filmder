899d98d7fb040492bfeb0733dd1836d917e085d1
Paolo <str237@gmail.com>
2020-10-22 11:20:34 +0200

    Render core context provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Search from './components/Search'
+import { CoreContextProvider } from './contexts/index'
 
 import './styles/Main.css'
 
 ReactDOM.render(
   <div>
-    <Search />
+    <CoreContextProvider>
+      <Search />
+    </CoreContextProvider>
     <div id="filmder-icon"></div>
   </div>,
   document.getElementById('root')
