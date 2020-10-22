4950f18fbd13b748a109b4317865d9926cef1046
Paolo <str237@gmail.com>
2020-10-22 11:15:23 +0200

    Storing name

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,7 +1,21 @@
+import { faSadCry } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 import store from 'store2'
+import { CORE_ACTIONS } from '../actions/actionNames'
+
+const actions = {
+  [CORE_ACTIONS.SET_USERNAME]: (state, { payload }) => {
+    const { name } = payload
+    store.set('username', name)
+    return { ...state, username: name }
+  }
+}
 
 const coreReducer = (state, action) => {
+  const fn = CORE_ACTIONS[action.type]
+  if (fn) {
+    state = fn(state, action)
+  }
   return state
 }
 
