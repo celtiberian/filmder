10056765dbb6459b9b1cf25e559272b50db454e1
Paolo <str237@gmail.com>
2020-10-22 11:09:08 +0200

    Init store

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import store from 'store2'
 
 const coreReducer = (state, action) => {
   return state
 }
 
 const init = () => {
-
+  const username = store.get('username') || ''
+  return {
+    username,
+  }
 }
 
 export const useCoreReducer = () => {
-   return React.useReducer(coreReducer, init)
+  return React.useReducer(coreReducer, init)
 }
