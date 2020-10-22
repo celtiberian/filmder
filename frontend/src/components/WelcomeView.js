59c4f4cf97efc5cd06fd8aa314e7a8d4dbce3ffb
Paolo <str237@gmail.com>
2020-10-22 11:39:54 +0200

    Base logic

--- frontend/src/components/WelcomeView.js
+++ frontend/src/components/WelcomeView.js
@@ -1,3 +1,24 @@
+import React from 'react'
+import { useCoreReducer } from '../reducers'
+import { setName } from '../actions'
+
 export const WelcomeView = () => {
-  return false
+  const [globalState, dispatch] = useCoreReducer()
+  const [name, setLocalName] = React.useState('')
+
+  const onChange = (evt) => {
+    setLocalName(evt.target.value)
+  }
+  const onSubmit = () => {
+    dispatch(setName(name))
+  }
+
+  return (
+    <div>
+      <h2>Welcome</h2>
+      <form onSubmit={onSubmit}>
+        <input required value={name} onChange={onChange} />
+      </form>
+    </div>
+  )
 }
