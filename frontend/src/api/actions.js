05d57ea22f669f60a83efadadc9f56d90959c67d
anne <anne.marie@strivelabs.io>
2020-10-22 12:11:31 +0200

    fix

--- frontend/src/api/actions.js
+++ frontend/src/api/actions.js
@@ -1,14 +1,10 @@
-import { endpoints } from "./endpoints";
-import API from "./commonAPI";
+import { endpoints } from './endpoints'
+import API from './commonAPI'
 
 const getMovieByID = async (movieID) => {
-  const res = await API.get(
-    endpoints.getMovieByID(movieID),
-  )
+  const res = await API.get(endpoints.getMovieByID(movieID))
   if (res.error) throw res.error
   return res.data
 }
 
-export {
-  getMovieByID,
-}
+export { getMovieByID }
