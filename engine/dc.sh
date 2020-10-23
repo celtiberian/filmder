287d133d2d5c12ac21aa8d79416ba65ab290f813
Paolo <str237@gmail.com>
2020-10-23 21:41:50 +0200

    Init machine learning engine folder

---
+++ engine/dc.sh
@@ -0,0 +1,5 @@
+docker-compose -f docker-compose.yml \
+    -f pgsql/docker-compose.base.yml \
+    -f pgsql/docker-compose.meta.yml \
+    -f pgsql/docker-compose.event.yml \
+    -f pgsql/docker-compose.model.yml $@
